79d941d6001696a064819c9a0d33839cf0a21695
kimyounghun1 <hunhoohunhoo1@gmail.com>
2018-11-20 01:40:46 +0900

    init commit

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+
+    <h1>CONTACT</h1>
+    
+    <form v-on:submit.prevent="onSubmit">
+      SEARCH : <input type="text" v-model="search"/><br/>
+      NAME : <input type="text" v-model="inputName"/>
+      PHONE NUMBER : <input type="text" v-model="inputNumber"/>
+      <input type="submit" value="ADD" v-on:click="addInput"/>
+    </form>
+
+    <p v-for="list in filtered" :key="list.id">
+      <Contact v-bind:name="list.name" v-bind:phoneNumber="list.phoneNumber"></Contact>
+    </p>
+
+  </div>
+</template>
+
+
+<script>
+
+import Contact from './components/Contact.vue'
+
+export default {
+  /*name: 'app',*/
+  components: {
+    Contact
+  },
+
+  data(){
+    return {
+      search:"",
+      inputName:"", 
+      inputNumber:"",
+      lists:[],
+      index:0
+    }
+  },
+
+  methods: {
+    addInput(){
+      this.index += 1
+      this.lists.push({
+        id:this.index,
+        name:this.inputName,
+        phoneNumber:this.inputNumber
+      })
+      
+      this.inputName = ""
+      this.inputNumber = ""
+    }
+  },
+
+  computed: {
+    filtered:function(){
+      let setFilter = this.search;
+        return this.lists.filter(function(list){
+          return list.name.includes(setFilter) || list.phoneNumber.includes(setFilter)
+        })
+    }
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>*/
---
+++ src/components/Contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <h2>{{ phoneNumber }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  /*name: 'Contact',*/
+  props: {
+    id:Number,
+    name:String,
+    phoneNumber:String
+  }
+}
+</script>
